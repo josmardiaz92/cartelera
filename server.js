ada2647abb7d434dd32295fdb41e263c977d1466
josmardiaz92 <josmardiaz92@gmail.com>
2023-06-29 11:33:44 -0400

    lipieza

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('sitio/html'));
-
-app.listen(3000, () => {
-  console.log('Servidor escuchando en el puerto 3000');
-});
