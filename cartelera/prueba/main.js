e56d3dcfe42756af4203973b7f57c67b009849e7
josmardiaz92 <josmardiaz92@gmail.com>
2023-07-07 11:02:29 -0400

    agregado de voz

--- cartelera/prueba/main.js
+++ cartelera/prueba/main.js
@@ -1,39 +1,79 @@
 $(document).ready(function() {
-    
-    const contenedor=document.getElementById('tabla-turnos');
+    let valorAnterior = '';
     // Función para realizar la solicitud AJAX
     function hacerSolicitud() {
         let c=0;
-    $.ajax({
-        url: 'db.php',
-        dataType: 'json',
-        success: function(data) {
-
-            const turnoActual=document.getElementById('turno-paciente-general').textContent=data[0].id;
-            const lugarActual=document.getElementById('turno-area-general').textContent=data[0].idCaja;
-            data.forEach(function(element, index) {
-                if (c < 5) {
-                    
-                    if (c === 0) {
-                        const turno1=document.getElementById('primerFilaTurno').textContent=element.id;
-                        const en1=document.getElementById('primerFilaEn').textContent=element.idCaja;
-                    } else {
-                        const turnoElement = document.getElementById(`turno${index + 1}`);
-                        const enElement = document.getElementById(`en${index + 1}`);
-                        turnoElement.textContent = element.id;
-                        enElement.textContent = element.idCaja;
-                    }
-                }
-            c++;
+        $.ajax({
+            url: 'db.php',
+            dataType: 'json',
+            success: function(data) {
+                const turnoActual=document.getElementById('turno-paciente-general').textContent=data[0].id;
+                const lugarActual=document.getElementById('turno-area-general').textContent=data[0].idCaja;
                 
-            });
-            /* console.log(data); */
-            // Manipula los datos recibidos como desees
-        },
-        error: function(xhr, status, error) {
-            console.error('Error en la solicitud AJAX:', status, error);
-        }
-    });
+                // Comparar el valor actual con el valor anterior
+                if (data[0].id !== valorAnterior) {
+                    const turno=document.getElementById('turnoActual');
+                    const entreturno=document.getElementById('entreTurnos');
+                    const textoaVoz=`Paciente ${data[0].id}, será atendido en el consultorio número ${data[0].idCaja}`;
+                    let i=0;
+                    for(i=0;i<3;i++) {
+                        aparecer(turno, entreturno);
+                        let utterance=new SpeechSynthesisUtterance(textoaVoz);
+                        speechSynthesis.speak(utterance);                    
+                    }
+                    setTimeout(() => {
+                        desaparecer(turno, entreturno);
+                    }, 15000);
+                    
+                    
+                    // Aquí puedes agregar cualquier acción que desees realizar cuando el valor cambie
+                }
+
+                // Almacenar el valor actual para la siguiente comparación
+                valorAnterior = data[0].id;
+
+                data.forEach(function(element, index) {
+                    if (c < 5) {
+                        
+                        if (c === 0) {
+                            const turno1=document.getElementById('primerFilaTurno').textContent=element.id;
+                            const en1=document.getElementById('primerFilaEn').textContent=element.idCaja;
+                        } else {
+                            const turnoElement = document.getElementById(`turno${index + 1}`);
+                            const enElement = document.getElementById(`en${index + 1}`);
+                            turnoElement.textContent = element.id;
+                            enElement.textContent = element.idCaja;
+                        }
+                    }
+                    c++;
+                });
+
+                function desaparecer(turno, entreturno) {
+                    turno.classList.remove("ver");
+                    turno.classList.add("no-ver");
+                    entreturno.classList.remove("no-ver");
+                    entreturno.classList.add("ver");
+                    setTimeout(() => {
+                        turno.classList.add("d-none");
+                        entreturno.classList.remove("d-none");
+                    }, 550);
+                }
+
+                function aparecer(turno, entreturno) {
+                    turno.classList.remove("no-ver");
+                    turno.classList.add("ver");
+                    entreturno.classList.remove("ver");
+                    entreturno.classList.add("no-ver");
+                    setTimeout(() => {
+                        turno.classList.remove("d-none");
+                        entreturno.classList.add("d-none");
+                    }, 550);
+                }
+            },
+            error: function(xhr, status, error) {
+                console.error('Error en la solicitud AJAX:', status, error);
+            }
+        });
     }
 
     // Ejecuta la función inicialmente
