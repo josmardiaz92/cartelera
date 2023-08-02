8d56b883e0cae449f50dc06c72380fdbf8481a66
josmardiaz92 <josmardiaz92@gmail.com>
2023-08-02 17:53:51 -0400

    terminar

---
+++ carteleraHSA/php/listarLab.php
@@ -0,0 +1,27 @@
+<?php
+$host = 'localhost';
+$database = 'cartelera';
+$user = 'supervisor';
+$password = 'imagenes1234';
+
+try {
+    $connection = new PDO("pgsql:host=$host;dbname=$database", $user, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $query = "select * from multimedia where are_mul='laboratorio'";
+    $statement = $connection->query($query);
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    // Cerrar la conexión
+    $connection = null;
+
+    // Convertir los datos a JSON
+    $jsonResult = json_encode($result);
+
+    // Enviar los datos JSON como respuesta
+    header('Content-Type: application/json');
+    echo $jsonResult;
+} catch (PDOException $e) {
+    echo json_encode(['error' => 'Error de conexión: ' . $e->getMessage()]);
+}
+?>
