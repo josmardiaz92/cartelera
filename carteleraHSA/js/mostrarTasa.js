cd27eb1e02ac4006866ed8d9557ec876fd2c86d9
josmardiaz92 <josmardiaz92@gmail.com>
2023-08-09 09:31:15 -0400

    arreglo de bug de tasa

--- carteleraHSA/js/mostrarTasa.js
+++ carteleraHSA/js/mostrarTasa.js
@@ -15,17 +15,17 @@ function consultarTasa() {
     fetch(rutaTasa)
         .then(res => res.json())
         .then(tasa => {
+            const contenedorTasa = document.getElementById('tasaDia');
             if (tasa.length > 0) {
-                const contenedorTasa = document.getElementById('tasaDia');
                 if (tasa[0].usd_cam !== dolarAnteior || tasa[0].cop_cam !== pesosAnterior) {
                     dolar = parseFloat(tasa[0].usd_cam);
                     pesos = parseFloat(tasa[0].cop_cam);
                     contenedorTasa.innerHTML = ` Tasa del día: <b>Dolar:</b> ${dolar.toFixed(2)} Bs. <b>Pesos:</b> ${pesos.toFixed(2)}. `;
-                }else{
-                    contenedorTasa.innerHTML='';
                 }
                 dolarAnteior = tasa[0].usd_cam;
                 pesosAnterior = tasa[0].cop_cam;
+            }else{
+                contenedorTasa.innerHTML='';
             }
         });
 }
